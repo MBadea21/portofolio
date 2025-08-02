a386158465c5c71e00f61a89b7d5f6b9eef1472c
Mihai Badea <mihai.badea9@gmail.com>
2025-08-02 18:32:35 +0100

    main layout for personal projects finished

--- src/pages/PersonalProjects.tsx
+++ src/pages/PersonalProjects.tsx
@@ -6,7 +6,8 @@ interface Project {
 	image_src: string | null
 }
 
-import website_picture from "../assets/website_image_temp.png"
+// import website_picture1 from "../assets/website_image_temp1.png"
+import website_picture2 from "../assets/website_image_temp2.png"
 import { FaGithub } from "react-icons/fa"
 
 export default function PersonalProjects() {
@@ -17,7 +18,7 @@ export default function PersonalProjects() {
 				"A website to showcase my web development skills and to provide easy access to my other projects.",
 			technologies_list: ["Vite", "React", "Tailwind", "DaisyUI"],
 			repository_url: "https://github.com/MBadea21/portofolio",
-			image_src: website_picture,
+			image_src: website_picture2,
 		},
 		{
 			title: "Test 1",
@@ -55,29 +56,34 @@ export default function PersonalProjects() {
 
 	return (
 		<div className="mx-auto p-3 lg:p-0">
-			<h1 className="pb-3 pl-2 text-4xl lg:p-5">Personal Projects</h1>
-			<ul className="flex flex-col space-y-10 lg:items-center lg:space-y-20">
+			<h1 className="mx-auto w-fit text-4xl lg:p-5">Personal Projects</h1>
+			<ul className="flex flex-col space-y-5 lg:items-center lg:space-y-20">
 				{projects.map((project, index) => (
 					<li
 						key={index}
-						className="bg-base-200 flex flex-col space-y-5 rounded-xl p-3 shadow-xl lg:w-4/7 lg:flex-row lg:space-x-30 lg:rounded-3xl lg:p-7"
+						className="bg-base-200 flex h-fit flex-col justify-between space-y-10 rounded-xl p-5 shadow-xl lg:h-[300px] lg:w-4/7 lg:flex-row lg:space-x-30 lg:rounded-3xl lg:p-7"
 					>
-						<div className="m-0 flex max-w-full flex-col justify-between">
-							<h2 className="text-xl font-semibold">{project.title}</h2>
+						<div className="m-0 flex flex-col space-y-5 pb-5 lg:w-2/3 lg:space-y-7 lg:pb-0">
+							<h2 className="text-2xl font-semibold">{project.title}</h2>
 							<p>{project.description}</p>
 							<p>
 								Technologies: <br /> {project.technologies_list.join(", ")}
 							</p>
 							{project.repository_url && (
 								<button className="bg-neutral align-center flex w-fit space-x-2 rounded-lg border border-gray-500 px-2 py-1">
-									<FaGithub size={20} />
+									<FaGithub size={20} className="my-auto" />
 									<a href={project.repository_url}>View on Github</a>
 								</button>
 							)}
 						</div>
-						{project.image_src && (
-							<img src={project.image_src} className=" w-1/2 border" />
-						)}
+						<div className="place-items-center lg:w-full">
+							{project.image_src && (
+								<img
+									src={project.image_src}
+									className="mx-auto object-scale-down"
+								/>
+							)}
+						</div>
 					</li>
 				))}
 			</ul>
