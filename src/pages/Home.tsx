8b61d5df44b5cf6e39bbf542fe8cc099bc14f6e9
Mihai Badea <mihai.badea9@gmail.com>
2025-08-02 19:22:19 +0100

    start adding skills and info

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,25 @@
 export default function Home() {
 	return (
-		<div className="mx-auto lg:max-w-screen-xl lg:pl-5">
-			<div>
+		<div className="mx-auto flex flex-col lg:max-w-screen-xl lg:flex-row lg:pl-5">
+			<div className="">
 				<h2>About me</h2>
-				<p>Bla Bla Bla</p>
+				<p>Multi-disclipline, Computer Savvy Individual</p>
+				<p>Technically Minded, Fast Learner</p>
 			</div>
 			<div>
 				<h2>Skills</h2>
 				<ul>
-					<li>Programming: Javascript, Typescript, Python</li>
-					<li>Web: React, Next, Vite</li>
+					<li>Programming languages: Javascript, Typescript, Python</li>
+					<li>
+						System Administration: VPS Management, Linux Shell, Basic
+						Networking, Docker Compose
+					</li>
+					<li>Web Technologies: React, Next, Vite, Tailwind</li>
+					<li>IoT and Web: MQTT, HTTPS</li>
+					<li>Databases: MySQL, SQLite, Prometheus</li>
+					<li>Game Engines: Godot, Unity</li>
 					<li>Image Editing: Photoshop, Gimp</li>
-					<li>3D: Blender</li>
+					<li>3D Modelling and Animation: Blender</li>
 				</ul>
 			</div>
 		</div>
